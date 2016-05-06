17e2c1485f3e5d7b7ea668c3a64a12e4e68188cf
Porter Smith <flowbish@gmail.com>
2016-05-06 05:59:58 -0500

    randomly named files

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,15 @@
 extern crate telegram_bot;
 extern crate hyper;
+extern crate rand;
 
+use rand::Rng;
 use std::thread::spawn;
 use std::env::var;
 use std::io;
 use std::fs::File;
 use std::path::{Path,PathBuf};
 use telegram_bot::{Api, MessageType, ListeningMethod, ListeningAction};
-use telegram_bot::types::User;
+use telegram_bot::types::{User,Integer};
 use hyper::Url;
 use hyper::method::Method;
 use hyper::client::{Request};
@@ -16,36 +18,87 @@ const ENV_TOKEN: &'static str = "BOT_TOKEN";
 const ENV_DOWNLOAD_DIR: &'static str = "DOWNLOAD_DIR";
 const ENV_BASE_URL: &'static str = "BASE_URL";
 
-fn download_file(download_dir: &Path, baseurl: &Url, url: &Url) -> io::Result<Url> {
+struct TGFile {
+    file_id: String,
+    file_size: Integer
+}
+
+impl TGFile {
+    fn from_message(msg: MessageType) -> Option<TGFile> {
+        if let Some((file_id, Some(file_size))) = match msg.clone() {
+            MessageType::Photo(photos) => {
+                let largest_photo = photos.last().unwrap();
+                Some((largest_photo.file_id.clone(), largest_photo.file_size))
+            },
+            MessageType::Sticker(sticker) => Some((sticker.file_id, sticker.file_size)),
+            MessageType::Document(document) => Some((document.file_id, document.file_size)),
+            MessageType::Audio(audio) => Some((audio.file_id, audio.file_size)),
+            MessageType::Video(video) => Some((video.file_id, video.file_size)),
+            MessageType::Voice(voice) => Some((voice.file_id, voice.file_size)),
+            _ => None
+        } {
+            Some(TGFile{ file_id: file_id, file_size: file_size})
+        }
+        else {
+            None
+        }
+    }
+}
+
+fn generate_name() -> String {
+    let mut rng = rand::thread_rng();
+    rng.gen_ascii_chars().take(6).collect()
+}
+
+fn replace_filename(filename: &str, name: &str) -> String {
+    match filename.split('.').last() {
+        Some(ext) => format!("{}.{}", name, ext),
+        None => name.into()
+    }
+}
+
+fn download_to_file(url: &Url, destination: &Path) -> io::Result<()>{
     // Create a request to download the file
     let req = Request::new(Method::Get, url.clone()).unwrap();
     let mut resp = req.start().unwrap().send().unwrap();
 
+    // Open file and copy downloaded data
+    let mut file = try!(File::create(destination));
+    try!(std::io::copy(&mut resp, &mut file));
+
+    Ok(())
+}
+
+fn download_file(url: &Url, destination: &Path, baseurl: &Url) -> io::Result<Url> {
     // Grab the last portion of the url
     let filename = url.path().unwrap().last().unwrap();
 
     // Create path by combining filename from url with download dir
-    let mut path = download_dir.to_path_buf();
-    path.push(filename);
+    let hash = generate_name();
+    let filename = replace_filename(&filename, &hash);
+    let mut path = destination.to_path_buf();
+    path.set_file_name(&filename);
 
-    // Open file and copy downloaded data
-    let mut file = try!(File::create(path));
-    std::io::copy(&mut resp, &mut file).unwrap();
+    try!(download_to_file(url, &path));
 
     // Create the return url that maps to this filename
-    let mut returl = baseurl.clone();
-    returl.path_mut().unwrap().push(filename.clone());
+    let returl = baseurl.join(&filename).unwrap();
     Ok(returl)
 }
 
-fn download_file_user(user: &User, base_download_dir: &Path, base_url: &Url, url: &Url) -> io::Result<Url> {
+fn download_file_user(url: &Url, user: &User, base_download_dir: &Path, base_url: &Url) -> io::Result<Url> {
+    // Create the final download directory by combining the base
+    // directory with the username, and ensure it exists.
     let mut download_dir_user = base_download_dir.to_path_buf();
     let user_path = user_path(&user);
     download_dir_user.push(user_path.clone());
     ensure_dir(&download_dir_user);
-    let mut base_url_user = base_url.clone();
-    base_url_user.path_mut().map(|p| p.push(user_path.clone()));
-    download_file(&download_dir_user, &base_url_user, &url)
+
+    // Create the final URL by combining the base URL and the
+    // username.
+    let base_url_user = base_url.join(&user_path).unwrap();
+
+    download_file(&url, &download_dir_user, &base_url_user)
 }
 
 fn ensure_dir(path: &Path) {
@@ -53,8 +106,8 @@ fn ensure_dir(path: &Path) {
 }
 
 fn user_path(user: &User) -> String {
-    match user.username.clone() {
-        Some(name) => name.clone(),
+    match user.username {
+        Some(ref name) => name.clone(),
         None => "anonymous".into()
     }
 }
@@ -80,27 +133,19 @@ fn main() {
     let tg_listener = spawn(move || {
         listener.listen(|u| {
             if let Some(m) = u.message {
-                let user = m.from;
+                let user = m.from.clone();
 
-                let file_id = match m.msg.clone() {
-                    MessageType::Photo(photos) => {
-                        let largest_photo = photos.last().unwrap();
-                        Some(largest_photo.file_id.clone())
-                    },
-                    MessageType::Sticker(sticker) => Some(sticker.file_id),
-                    MessageType::Document(document) => Some(document.file_id),
-                    MessageType::Audio(audio) => Some(audio.file_id),
-                    MessageType::Video(video) => Some(video.file_id),
-                    MessageType::Voice(voice) => Some(voice.file_id),
-                    _ => None
-                };
 
-                // Handle media (files) sent to us directly.
-                if let Some(file_id) = file_id {
-                    let file = api.get_file(&file_id).unwrap();
+                // Attempt to download the file to the user's subdirectory
+                if let Some(tgfile) = TGFile::from_message(m.msg.clone()) {
+                    let file = api.get_file(&tgfile.file_id).unwrap();
                     if let Some(path) = file.file_path {
+                        // Get the file's URL on Telegram's server
                         let tg_url = Url::parse(&api.get_file_url(&path)).unwrap();
-                        let client_url = download_file_user(&user, &download_dir, &base_url, &tg_url).unwrap();
+
+                        // Download the final file and create the URL for the user
+                        let client_url = download_file_user(&tg_url, &user, &download_dir, &base_url).unwrap();
+                        println!("[INFO] {} direct upload {} ({})", user_path(&user), client_url, tgfile.file_size);
                         let _ = api.send_message(
                             m.chat.id(),
                             format!("{}", client_url),
@@ -111,7 +156,9 @@ fn main() {
                 // Handle URLs sent to us for rehosting.
                  if let MessageType::Text(txt) = m.msg {
                      if let Ok(url) = Url::parse(&txt) {
-                         let client_url = download_file_user(&user, &download_dir, &base_url, &url).unwrap();
+                        // Download the final file and create the URL for the user
+                         let client_url = download_file_user(&url, &user, &download_dir, &base_url).unwrap();
+                         println!("[INFO] {} rehost {} from {}", user_path(&user), client_url, url);
                          let _ = api.send_message(
                              m.chat.id(),
                              format!("{}", client_url),
